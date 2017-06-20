2befd111159459f184fcdc28cc447924d8673a9c
fifths <ligangmingx@gmail.com>
2017-06-20 17:59:41 +0800

    update router

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -9,37 +9,43 @@ class Router
     public static $callbacks = [];
     public static $error_callback;
 
-    /**
-     * @param $name 请求类型
-     * @param $arguments
-     */
     public static function __callstatic($name, $arguments)
     {
         //请求地址
         $uri = $arguments[0];
+        //路由
         array_push(self::$routes, $uri);
+        //请求类型|方法
         array_push(self::$methods, strtoupper($name));
+        //准备执行的方法
         array_push(self::$callbacks, $arguments[1]);
     }
 
     public static function dispatch()
     {
         try {
+            //uri
             $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+            //请求类型
             $method = $_SERVER['REQUEST_METHOD'];
             if (in_array($uri, self::$routes)) {
                 $route_pos = array_keys(self::$routes, $uri);
                 foreach ($route_pos as $route) {
                     if (self::$methods[$route] == $method || self::$methods[$route] == 'ANY') {
                         if (is_object(self::$callbacks[$route])) {
+                            // do function
                             return call_user_func(self::$callbacks[$route]);
                         } else {
                             $segments = explode('@', self::$callbacks[$route]);
+                            // new ob
                             $controller = new $segments[0]();
                             return $controller->{$segments[1]}();
                         }
                     }
                 }
+            }else{
+                //regex with routes
+
             }
             if (self::$error_callback) {
                 self::get($_SERVER['REQUEST_URI'], self::$error_callback);
